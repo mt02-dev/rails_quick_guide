d1ff6b3412cc8bf31f80e2623ddc1e318e64cd45
mt02-dev <t.m.215soccer@icloud.com>
2024-01-17 01:48:02 +0900

    管理者権限用ユーザ作成サイト作成

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,56 @@
+class Admin::UsersController < ApplicationController
+  def new
+    @user = User.new(flash[:user])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
+    else
+      redirect_back fallback_location: @user, flash: {
+        user: @user,
+        error_messages: @user.errors.full_messages
+      }
+    end
+
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redire_to admin_user_path(@user), notice: "ユーザ「#{user.name}」を登録しました。"
+    else
+      redirect_back fallback_location: @user, flash: {
+        user: @user,
+        error_messages: @user.errors.full_messages
+      }
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_user_path, status: :see_other, notice: "ユーザ「#{@user.name}」を削除しました。"
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_comfirmation)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_16_061226) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_16_085402) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_16_061226) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
