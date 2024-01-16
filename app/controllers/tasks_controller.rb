40490b6f0acef51ea2a71acd34b60c2cdec33413
mt02-dev <t.m.215soccer@icloud.com>
2024-01-16 14:57:07 +0900

    基本的なCRUD機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,55 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
+
   end
 
   def new
+    @task = Task.new(flash[:task])
   end
 
-  def edit
+  def create
+    task = Task.new(task_params)
+    if task.save
+      flash[:notice] = "タスク名:#{task.name}を登録しました。"
+      redirect_to tasks_path(:task)
+      # redirect_to tasks_path, notice: "タスク名:#{task.name}を登録しました。"
+    else
+      redirect_back fallback_location: new_user_path, flash: {
+        task: task,
+        error_messages: task.errors.fullmessages
+      }
+    end
   end
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    if task.update(task_params)
+      redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+    else
+      redirect_back fallback_location: edit_task_path, flash: {
+        task: task,
+        error_messages: task.errors.fullmessages
+      }
+    end
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, status: :see_other, notice: "タスク「#{task.name}」を削除しました。"
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module Taskleaf
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    # config.i18n.default_local = :ja
+    config.i18n.default_locale = :ja
   end
 end
